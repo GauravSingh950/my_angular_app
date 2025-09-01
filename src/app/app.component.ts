a0e2f50dde024e73c7be2305913e221c1041f5da
Gaurav Kumar <gaurav.kumar.4@maersk.com>
2025-09-01 12:41:16 +0530

    For Loop in Template File

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,11 @@ import { ProfileComponent } from './profile/profile.component';
   styleUrls: ['./app.component.css'] // Fixed typo
 })
 export class AppComponent {
-  color = 'none';
-  handleInput(event:Event){
-    this.color = (event.target as HTMLInputElement).value;
-  }
+  employees = ["Gaurav","Kushal","Srushtitha"];
+  students = [
+    { id: 1, name: 'John', age: 20 },
+    { id: 2, name: 'Jane', age: 22 },
+    { id: 3, name: 'Mike', age: 21 },
+    { id: 4, name: 'Emma', age: 23 }
+  ]
 }
