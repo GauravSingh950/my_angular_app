1ce216d4607c61d1cafcf90ea58de0f00bb640e6
Gaurav Kumar <gaurav.kumar.4@maersk.com>
2025-09-02 17:08:18 +0530

    Made Maersk Todo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
-import { Component, effect, signal } from '@angular/core';
-import { LoginComponent } from "./login/login.component";
-import { ProfileComponent } from './profile/profile.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+interface Todo{
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, ProfileComponent], // Removed RouterOutlet
+  imports: [CommonModule, FormsModule], // Added CommonModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // Fixed typo
 })
 export class AppComponent {
-  count = signal(10);
-  x = 20;
+  name = ""; 
+  todo: Todo[] = [];
+  addTodo(taskName: string){
+    if(taskName.trim()){
+      this.todo.push({
+        id: Date.now(),
+        text: taskName,
+        completed: false
+      });
+    }
+    console.log(new Date())
+  } 
+  completeTodo(id:number){
+    const todo = this.todo.find(t => t.id === id);
+    if(todo){
+      todo.completed = true;
+    }
+  }
+  removeTodo(id: number){
+    this.todo = this.todo.filter(t=>t.id!=id);
+  }
+  resetTodo(){
+    this.todo = new Array<Todo>();
+  }
 
-  constructor(){
-    effect(()=>{
-      console.log(this.count());
-    })
-  }
-  updateValue(){
-    this.count.set(this.count() + 1);
-  }
 }
