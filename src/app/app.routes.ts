871c64eaeed7421d55c7121a89bbb944e149dcc2
Gaurav Kumar <gaurav.kumar.4@maersk.com>
2025-09-03 23:39:06 +0530

    404Page Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
-    {path:'header',component:HeaderComponent}
-    
+    {path:'header',component:HeaderComponent},
+    {path:'**', component: PageNotFoundComponent} // Wildcard
 ];
+
