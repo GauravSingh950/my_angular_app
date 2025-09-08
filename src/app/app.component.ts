83d2387fb33e0324989de4f8da551b92d70616ab
Gaurav Kumar <gaurav.kumar.4@maersk.com>
2025-09-08 19:58:44 +0530

    Passing Data from Parent to Child Comp.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,18 @@
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators, ValueChangeEvent } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { UserComponent } from './user/user.component';
   
 
   @Component({
     selector: 'app-root',
+    standalone:true,
     imports: [RouterLink,RouterOutlet,RouterLinkActive,PageNotFoundComponent,HeaderComponent,
-              ReactiveFormsModule,NgIf,FormsModule
+              ReactiveFormsModule,NgIf,FormsModule, UserComponent
               ], 
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
   })
   export class AppComponent {
-    addUserDetails(val: NgForm){
-      console.log(val);
-    }
+    selectedUser: string = '';
   }
