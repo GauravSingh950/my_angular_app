a870e5f96008213f8eb0afd5ae1d9f4a85703bc8
Gaurav Kumar <gaurav.kumar.4@maersk.com>
2025-09-08 15:14:57 +0530

    Template-Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,20 @@
   import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators, ValueChangeEvent } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators, ValueChangeEvent } from '@angular/forms';
 import { NgIf } from '@angular/common';
   
 
   @Component({
     selector: 'app-root',
     imports: [RouterLink,RouterOutlet,RouterLinkActive,PageNotFoundComponent,HeaderComponent,
-              ReactiveFormsModule,NgIf
+              ReactiveFormsModule,NgIf,FormsModule
               ], 
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
   })
   export class AppComponent {
-    
-    profileForm = new FormGroup({
-      name: new FormControl('',[Validators.required]),
-      password : new FormControl('',[Validators.required,Validators.minLength(6)]),
-      email: new FormControl('',[Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])
-    });
-    onSubmit(){
-      console.log(this.profileForm.value);
-    }
-
-    get name(){
-      return this.profileForm.get('name');
-    }
-    get password(){
-      return this.profileForm.get('password');
-    }
-    get email(){
-      return this.profileForm.get('email');
+    addUserDetails(val: NgForm){
+      console.log(val);
     }
   }
